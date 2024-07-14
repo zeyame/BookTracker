0d14091390cb7f845df02546aaad4e5c83f7f57e
zeyame <zeyame14@gmail.com>
2024-07-14 11:10:43 +0300

    Initial navbar design completed

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -11,8 +11,8 @@ interface NavButtonProps {
 export const NavButton: React.FC<NavButtonProps> = ({id, name, xmlns, color, path}) => {
     return (
         <li id={id} className="nav-option">
-            <a>
-                <svg className="search-icon" xmlns={xmlns} width='11' height='11' viewBox='0 0 50 50' fill={color}>
+            <a className="nav-link">
+                <svg className="nav-icon" xmlns={xmlns} width='12' height='12' viewBox='0 0 50 50' fill={color ? `#${color}` : undefined}>
                     <path d={path} />
                 </svg>
                 <p>
