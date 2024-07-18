bf68f1dfec1e55e96bdde3b0e58c982da7054343
zeyame <zeyame14@gmail.com>
2024-07-18 12:36:45 +0300

    Backend code now more concise with a dynamic route for the endpoint of fetching books in  a genre

--- Backend/app.py
+++ Backend/app.py
@@ -11,37 +11,7 @@ COVERS_URL = "https://covers.openlibrary.org/b/id"      # add the cover id, the 
 
 
 # SETTING UP THE ENDPOINTS FOR THE BACKEND
-
-@app.route('/romance-books', methods=['GET'])
-def getRomanceBooks():
-    return getBooksByGenre('romance')
-    
-@app.route('/fiction-books', methods=['GET'])
-def getFictionBooks():
-    return getBooksByGenre('fiction')
-
-@app.route('/thriller-books', methods=['GET'])
-def getThrillerBooks():
-    return getBooksByGenre('thriller')
-
-@app.route('/action-books', methods=['GET'])
-def getActionBooks():
-    return getBooksByGenre('action')
-
-@app.route('/mystery-books', methods=['GET'])
-def getMysteryBooks():
-    return getBooksByGenre('mystery')
-
-@app.route('/history-books', methods=['GET'])
-def getHistoryBooks():
-    return getBooksByGenre('history')
-
-@app.route('/scifi-books', methods=['GET'])
-def getScifiBooks():
-    return getBooksByGenre('science_fiction')
-
-
-# helper function
+@app.route('/<genre>-books', methods=['GET'])
 def getBooksByGenre(genre):
     limit = request.args.get('limit', 20)
     
@@ -50,7 +20,7 @@ def getBooksByGenre(genre):
     
     # we send back an error if request failed
     if response.status_code != 200:
-        return jsonify({'error': 'An error occurred requesting books with subject: love'}), response.status_code
+        return jsonify({'error': f'An error occurred requesting books with subject: {genre}'}), response.status_code
     
     data = response.json()      # a python dictionary of 5 key value pairs
     works = data.get('works', [])       # gets a python list of dictionaries of the 20 books, each dictionary storing metadata about a specific book
@@ -61,13 +31,17 @@ def getBooksByGenre(genre):
     
     return jsonify(books)
 
+
 # helper function
 def formattedBooks(works):
     result = []
     if works:
         for work in works:
-            # extracting the book name and cover id from metadata dictionary of each book
-            name, cover_id, author = work.get('title', ''), work.get('cover_id', ''), work.get('authors')[0].get('name')
+            # extracting the book name, cover id and author from metadata dictionary of each book
+            name = work.get('title', '')
+            cover_id = work.get('cover_id', 0)
+            authors = work.get('authors', [])
+            author = authors[0].get('name') if authors else 'Unknown'
             
             # we get a jpg image url for each book
             image_url = f'{COVERS_URL}/{cover_id}-S.jpg' if cover_id else None
