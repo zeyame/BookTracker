e24c64a986fcb702518e0c492de6da4ceaa616ba
zeyame <zeyame14@gmail.com>
2024-07-22 16:34:04 +0300

    Fixed some design bugs and added the right arrow svg to be used for pagination

--- Frontend/src/components/Genre.tsx
+++ Frontend/src/components/Genre.tsx
@@ -18,7 +18,7 @@ export const Genre: React.FC<GenreProps> = ( { name, books } ) => {
                 )}
                 {booksLength > 0 && 
                     <div className="load-more-icon">
-                        <svg fill="#000000" height="10" width="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+                        <svg fill="#000000" height="100" width="100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
                             <path d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
                                 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
                                 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
