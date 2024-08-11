11813ee3f02a22637851c29dc8a8d811a281cb49
zeyame <zeyame14@gmail.com>
2024-08-11 22:18:16 +0300

    Similar books history bug of adding duplicated array of similar books at the beginning fixed.

--- Frontend/src/pages/BookPage.tsx
+++ Frontend/src/pages/BookPage.tsx
@@ -49,6 +49,7 @@ export const BookPage: React.FC = () => {
         const [similarBooks, setSimilarBooks] = useState<Array<book>>([]);
         const [similarBooksHistory, setSimilarBooksHistory] = useState<Array<Array<book>>>([]);
         const [bookDescription, setBookDescription] = useState<string>('');
+        console.log(similarBooksHistory);
 
         // refs 
         const fullAuthorDescriptionRef = useRef<string>('');
@@ -137,7 +138,7 @@ export const BookPage: React.FC = () => {
                             const newSimilarBooks = similarBooksCacheRef.current.slice(-5);
 
                             // updating history
-                            setSimilarBooksHistory(prevHistory => [...prevHistory, newSimilarBooks]);
+                            setSimilarBooksHistory([newSimilarBooks]);
                             setSimilarBooks(newSimilarBooks);
                             similarBooksCacheRef.current.splice(-5);
                         }
@@ -151,16 +152,16 @@ export const BookPage: React.FC = () => {
                         if (similarBooks && similarBooks.length > 0) {
                             similarBooksCacheRef.current = similarBooks;
 
-                            const newSimilarBooks = similarBooksCacheRef.current.slice(-5);
+                            const newSimilarBooks = similarBooksCacheRef.current.slice(0, 5);
                             
                             // updating history
-                            setSimilarBooksHistory(prevHistory => [...prevHistory, newSimilarBooks]);
+                            setSimilarBooksHistory([newSimilarBooks]);
 
                             setSimilarBooks(newSimilarBooks);
 
                             // saving full cache to storage and removing the retrieved books 
                             sessionStorage.setItem(`${book.title}-similar-books`, JSON.stringify(similarBooksCacheRef.current));
-                            similarBooksCacheRef.current.splice(-5);
+                            similarBooksCacheRef.current.splice(0, 5);
                         }
                     }
                 }   
@@ -181,6 +182,7 @@ export const BookPage: React.FC = () => {
         getSimilarBooks();
         return () => {
             setSimilarBooks([]);
+            setSimilarBooksHistory([]);
         }
     }, [book]);
 
@@ -263,26 +265,29 @@ export const BookPage: React.FC = () => {
         }
     }
 
-    // gets 5 similar books from the history
+    // gets the last 5 similar books from the history
     const handleLeftArrowClick = () => {
         if (similarBooksHistory.length > 0) {
-            // sending back the currently displayed books to the cache
-            similarBooks.forEach(book => similarBooksCacheRef.current.push(book));
             // updating history
             const newHistory = similarBooksHistory.slice(0, -1);
-            const lastSimilarBooks = newHistory[newHistory.length-1];
-            setSimilarBooks(lastSimilarBooks);
-            setSimilarBooksHistory(newHistory);
+
+            if (newHistory.length > 0) {
+                // sending back current similar books to the cache
+                similarBooksCacheRef.current.unshift(...similarBooks);
+                const lastSimilarBooks = newHistory[newHistory.length-1];
+                setSimilarBooks(lastSimilarBooks);
+                setSimilarBooksHistory(newHistory);
+            }
         }
     }
 
-    // gets 5 similar books from the cache
+    // gets the last 5 similar books from the cache
     const handleRightArrowClick = () => {
         if (similarBooksCacheRef.current.length > 0) {
-            const newSimilarBooks = similarBooksCacheRef.current.slice(-5);
+            const newSimilarBooks = similarBooksCacheRef.current.slice(0, 5);
             setSimilarBooksHistory(prevHistory => [...prevHistory, newSimilarBooks]);
             setSimilarBooks(newSimilarBooks);
-            similarBooksCacheRef.current.splice(-5);
+            similarBooksCacheRef.current.splice(0, 5);
         }
     }
 
