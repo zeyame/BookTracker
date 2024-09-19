300fa6ced3fac0c8197e3fd2b22158a5f6a6945f
zeyame <zeyame14@gmail.com>
2024-09-19 20:11:09 +0300

    Designed verification page

--- client/src/pages/account/VerificationPage.tsx
+++ client/src/pages/account/VerificationPage.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import '../../styles/verification-page.css';
 
 export const VerificationPage: React.FC = () => {
+
+    const location = useLocation();
+    const email: string | null = location.state.email;
+
     return (
-        <h1>
-            Verification Page
-        </h1>
+        <div className="verification-page-container">
+            <h1 className="verification-page-app-name">Shelf Quest</h1>
+            <div className="verification-container">
+                <h2 className="verification-page-title">Verify email address</h2> 
+                <p className="verification-message">To verify your email , we've sent a One Time Password (OTP) to {email}</p>
+                <div className="otp-input-container">
+                    <input className="otp-input" placeholder="Enter OTP"  />
+                </div>
+                <button className="submit-otp-button">
+                    Create account
+                </button>
+                <p className="resend-otp">Resend OTP</p>
+            </div>
+        </div>
     );
 }
